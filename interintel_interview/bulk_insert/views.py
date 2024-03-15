ed8e281cea12acd2bc8dfa2de590f1edb5f16efe
lawrence-254 <lodhiambo26@gmail.com>
2024-03-15 18:59:15 +0300

    modelssetup

--- interintel_interview/bulk_insert/views.py
+++ interintel_interview/bulk_insert/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product, ProductVariant
 
 # Create your views here.
 def home(request):
-    return render(request, 'bulk_insert/index.html')
+    context = {
+        'products': Product.objects.all(),
+        'variants': ProductVariant.objects.all()
+    }
+    return render(request, 'bulk_insert/index.html', context)
 
 def insert(request):
-    return render(request, 'bulk_insert/insert.html')
+    context = {
+        'products': Product.objects.all(),
+        'variants': ProductVariant.objects.all()
+    }
+    return render(request, 'bulk_insert/insert.html', context)
 
 def insert_variant(request):
-    return render(request, 'bulk_insert/insert_variant.html')
+    context = {
+        'products': Product.objects.all(),
+        'variants': ProductVariant.objects.all()
+    }
+    return render(request, 'bulk_insert/insert_variant.html', context)
